f8555b0445ed4af3f93397da4b3b5063ac854d19
IKECHUKWU <sundayjabikem@gmail.com>
2023-01-03 21:28:26 +0100

    Udate

--- Ordering Service/Ordering.Domain/Entities/Order.cs
+++ Ordering Service/Ordering.Domain/Entities/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ordering.Domain.CommonEntities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,32 @@ using System.Threading.Tasks;
 
 namespace Ordering.Domain.Entities
 {
-    public class Order
+    public class Order:EntityBase
     {
-        public Guid Id { get; set; }
+        public Guid OderId { get; set; }
+        //public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime OrderDate { get; set; }
+        public List<object>? OrderedItems { get; set; }
+
+
+        // BillingAddress
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string AddressLine { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string? ZipCode { get; set; }
+
+        // Payment
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string CVV { get; set; }
+        public int PaymentMethod { get; set; }
+
+
+
     }
 }
