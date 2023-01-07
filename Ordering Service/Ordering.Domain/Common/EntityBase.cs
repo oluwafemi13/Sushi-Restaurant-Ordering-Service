2cd3c332de9fdaa68c84ec5f0cf290927f8d822c
IKECHUKWU <sundayjabikem@gmail.com>
2023-01-07 16:16:55 +0100

    Made Changes to the test

---
+++ Ordering Service/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Common
+{
+    public class EntityBase
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
