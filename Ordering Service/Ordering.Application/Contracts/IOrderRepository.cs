2873be4ad13754c4da14dbdf64952a77fa4e4121
IKECHUKWU <sundayjabikem@gmail.com>
2023-01-26 19:56:44 +0100

    update

--- Ordering Service/Ordering.Application/Contracts/IOrderRepository.cs
+++ Ordering Service/Ordering.Application/Contracts/IOrderRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ordering.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace Ordering.Application.Contracts
 {
-    internal class IOrderRepository
+    public interface IOrderRepository
     {
+        Task<IEnumerable<Order>> GetOrdersByUsername(string username);
+
     }
 }
